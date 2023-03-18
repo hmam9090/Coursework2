2000b042243c57a6d4562444c54c7125c8fbc7ae
hamam9090 <hh823@live.mdx.ac.uk>
2023-03-18 14:22:41 +0000

    Adding a method that divides the words into halves by recursion till we get to two words only

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,17 @@ public class Main {
         String[] words = {"Dopey", "Doc", "Sleepy", "Happy", "Bashful", "Sneezy","Grumpy"};
 
     }
+    public static void mergeSort(String[] a, int from, int to) {
+        if (from == to) {
+            return;
+        }
+        int mid = (from + to) / 2;
+        // sort the first half
+        mergeSort(a, from, mid);
+        // sort the second half
+        mergeSort(a, mid + 1, to);
+
+    }// end mergeSort
+
 
 }
